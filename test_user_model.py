4bac8c4a9aaa90385b02e4959e2444a2e1a21017
shamsi <sarah.shamsi90@gmail.com>
2023-12-08 23:35:43 -0800

    Added user model test cases

--- test_user_model.py
+++ test_user_model.py
@@ -39,20 +39,110 @@ class UserModelTestCase(TestCase):
         Message.query.delete()
         Follows.query.delete()
 
-        self.client = app.test_client()
-
-    def test_user_model(self):
-        """Does basic model work?"""
-
-        u = User(
-            email="test@test.com",
-            username="testuser",
+        user1 = User(
+            email="testuser1@test.com",
+            username="testuser1",
             password="HASHED_PASSWORD"
         )
 
-        db.session.add(u)
+        db.session.add(user1)
+        db.session.commit()
+
+        user2 = User(
+            email="testuser2@test.com",
+            username="testuser2",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(user2)
+        db.session.commit()
+
+        user5 = User(
+            email="testuser5@test.com",
+            username="testuser5",
+            password="password"
+        )
+
+        uuid5 = 5555
+        user5.id = uuid5
+        db.session.commit()
+
+        self.user1 = user1
+        self.user2 = user2
+        self.user5 = user5
+
+        self.client = app.test_client()
+
+    
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
+
+        
+
+    def test_user_model(self):
+        """Does basic model work?"""
+        self.user1.following.append(self.user2);
         db.session.commit()
 
         # User should have no messages & no followers
-        self.assertEqual(len(u.messages), 0)
-        self.assertEqual(len(u.followers), 0)
+        self.assertEqual(len(self.user1.messages), 0)
+        self.assertEqual(len(self.user1.followers), 0)
+        self.assertEqual(self.user1.__repr__(),f'<User #{self.user1.id}: testuser1, testuser1@test.com>')
+        self.assertIs(self.user1.is_following(self.user2), True)
+        self.assertIs(self.user2.is_followed_by(self.user1), True)
+
+        self.user1.following.remove(self.user2);
+        db.session.commit()
+
+        self.assertIs(self.user1.is_following(self.user2), False)
+        self.assertIs(self.user2.is_followed_by(self.user1), False)
+
+    def test_user_create_valid_creds(self):
+
+        user_test = User.signup('user_test','usertest@f.com','password', None, 'canada','Hi Its life',None)
+        uuid = 1111
+        user_test.id = uuid
+
+        db.session.commit()
+
+        user_test = User.query.get(uuid);
+        self.assertIsNotNone(user_test)
+        self.assertEqual(user_test.username, 'user_test');
+        self.assertEqual(user_test.email, 'usertest@f.com');
+        self.assertNotEqual(user_test.password, 'password');
+        self.assertTrue(user_test.password.startswith, '$2b$');
+
+
+    def test_user_create_invalid_creds(self):
+
+        user_test_invalid = User.signup(None,'usertest3@f.com','password', None, 'canada','Hi Its life',None)
+        uuid = 1256788955
+        user_test_invalid.id = uuid
+
+        db.session.commit()
+
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+
+    def test_user_authenticate(self):
+
+        test = User.authenticate(self.user5.username, "password")
+
+        self.assertIsNotNone(test)
+        self.assertEqual(self.user5.id, 5555)
+
+    def test_user_authenticate_invalid_username(self):
+
+        self.assertFalse(User.authenticate("baduser", "password"))
+
+    
+    def test_user_authenticate_invalid_password(self):
+
+        self.assertFalse(User.authenticate(self.user5.username, "wrongpassword"))
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
